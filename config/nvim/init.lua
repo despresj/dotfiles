80faad013ad02ac6924fa4c39ea97b12d8b14ed1
despresj <54477023+despresj@users.noreply.github.com>
2023-01-29 13:35:13 -0500

    add diagnostic configs

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -63,13 +63,13 @@ local sign = function(opts)
   })
 end
 
-sign({name = 'DiagnosticSignError', text = ''})
-sign({name = 'DiagnosticSignWarn', text = ''})
+sign({name = 'DiagnosticSignError', text = '!!'})
+sign({name = 'DiagnosticSignWarn', text = '®'})
 sign({name = 'DiagnosticSignHint', text = ''})
 sign({name = 'DiagnosticSignInfo', text = ''})
 
 vim.diagnostic.config({
-    virtual_text = false,
+    virtual_text = true, -- display diagnostics
     signs = true,
     update_in_insert = true,
     underline = true,
