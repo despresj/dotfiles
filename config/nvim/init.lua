254c4e5968ef6cbbb344785946e320d2358e0083
despresj <54477023+despresj@users.noreply.github.com>
2023-01-29 15:54:06 -0500

    disable arrows

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -258,6 +258,11 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- consider this for changing permission via leader x
 --vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+-- diable arrows
+vim.keymap.set({ "n", "v" }, "<Up>", "<NOP>")
+vim.keymap.set({ "n", "v" }, "<Down>", "<NOP>")
+vim.keymap.set({ "n", "v" }, "<Left>", "<NOP>")
+vim.keymap.set({ "n", "v" }, "<Right>", "<NOP>")
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
