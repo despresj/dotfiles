55452c2d1579653aee7f9e8ac4d6c0c576f53b8c
despresj <54477023+despresj@users.noreply.github.com>
2023-02-04 21:47:47 -0500

    rm dupe lib

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -129,10 +129,6 @@ require("packer").startup(function(use)
 set signcolumn=yes
 autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
 ]] )
-  use({ -- Autocompletion
-    "hrsh7th/nvim-cmp",
-    requires = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip" },
-  })
 
   use({ -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
