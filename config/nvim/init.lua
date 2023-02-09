9c21f599bed2be313f6ad628184979cca7ade556
despresj <54477023+despresj@users.noreply.github.com>
2023-02-08 20:01:15 -0500

    add autopairs

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -31,6 +31,7 @@ require("packer").startup(function(use)
   use("hrsh7th/vim-vsnip")
   use("L3MON4D3/LuaSnip")
 
+  use("windwp/nvim-autopairs")
   -- require("luasnip.loaders.from_vscode").lazy_load()
 
   use({ -- LSP Configuration & Plugins
@@ -306,7 +307,38 @@ require("lualine").setup({
     section_separators = "",
   },
 })
+local status_ok, npairs = pcall(require, "nvim-autopairs")
+if not status_ok then
+  return
+end
 
+npairs.setup({
+  check_ts = true,
+  ts_config = {
+    lua = { "string", "source" },
+    javascript = { "string", "template_string" },
+    java = false,
+  },
+  disable_filetype = { "TelescopePrompt", "spectre_panel" },
+  fast_wrap = {
+    map = "<M-e>",
+    chars = { "{", "[", "(", '"', "'" },
+    pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+    offset = 0, -- Offset from pattern match
+    end_key = "$",
+    keys = "qwertyuiopzxcvbnmasdfghjkl",
+    check_comma = true,
+    highlight = "PmenuSel",
+    highlight_grey = "LineNr",
+  },
+})
+
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+  return
+end
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
 -- Enable Comment.nvim
 require("Comment").setup()
 
