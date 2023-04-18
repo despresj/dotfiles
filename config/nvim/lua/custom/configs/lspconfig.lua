38c1acfcac3f9e3242a90ef14f133b49f35c972e
despresj <54477023+despresj@users.noreply.github.com>
2023-04-17 21:10:06 -0400

    catchup

--- config/nvim/lua/custom/configs/lspconfig.lua
+++ config/nvim/lua/custom/configs/lspconfig.lua
@@ -3,3 +3,21 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
 local util = require "lspconfig/util"
 
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,require,
+  capabilities = capabilities,
+  filetypes = {"rust"},
+  root_dir = util.root_pattern("Cargo.toml"),
+  settings = {
+['rust_analyzer'] = {
+       assist = {
+        importEnforceGranularity = true,
+        importPrefix = "crate",
+      },
+    cargo = {
+      allFeatures = true,
+    }
+  }
+  }
+})
+
